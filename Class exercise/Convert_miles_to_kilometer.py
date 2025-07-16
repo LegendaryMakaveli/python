30ac6850aa0b7cd101fe4c07db5a40803994467c
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2025-07-16 15:41:59 +0100

    fuel budget calculator

---
+++ Class exercise/Convert_miles_to_kilometer.py
@@ -0,0 +1,8 @@
+miles = 175
+
+MILES_PER_KILOMETER = 1.609
+
+
+convert_miles_kilometer = miles * MILES_PER_KILOMETER
+
+print(convert_miles_kilometer)
---
+++ Class exercise/Fuel_budget.py
@@ -0,0 +1,7 @@
+budget = float(input("Enter your budgeted amount  "))
+
+PRICE_PER_LITERS = 855
+
+total = budget / PRICE_PER_LITERS
+
+print("You can buy", total ,  "liters of fuel with that amount")
