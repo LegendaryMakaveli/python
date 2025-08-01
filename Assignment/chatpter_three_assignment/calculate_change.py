af5cf84571518447439933522b0378454ae56ed4
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2025-08-01 07:02:31 +0100

    calculate change

---
+++ Assignment/chatpter_three_assignment/calculate_change.py
@@ -0,0 +1,7 @@
+amount_paid = float(input("Enter the amount paid:  "))
+purchase_price =float(input("Enter the purchase price:  "))
+
+
+change = amount_paid - purchase_price
+
+print( "Your change is ", change, "naira only")
