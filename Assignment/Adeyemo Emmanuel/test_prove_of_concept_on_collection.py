239ace1e79920db4211dc70a31f0a37a0d9bda60
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2025-08-17 04:23:45 +0100

    test file updated

--- Assignment/Adeyemo Emmanuel/test_prove_of_concept_on_collection.py
+++ Assignment/Adeyemo Emmanuel/test_prove_of_concept_on_collection.py
@@ -77,9 +77,122 @@ class TestForNumberThatIsDivisibleBy3AndBy5(TestCase) :
 		makaveli.assertRaises(ValueError,  prove_of_concept_on_collection.is_range, -1)
 
 	def test_for_function_functionality(makaveli) :
-		result = prove_of_concept_on_collection.is_range ([1,2,3,4,5,6,7,8,15])
-		makaveli.assertEqual(result, 15)
+		makaveli.assertTrue( prove_of_concept_on_collection.is_range (15))
+		makaveli.assertFalse( prove_of_concept_on_collection.is_range (10))
+
+
+
+
+class TestForFilteringAllNumberThatCanAreDivisibleBy3And5(TestCase) :
+	def test_that_the_function_exist(makaveli) :
+		prove_of_concept_on_collection.filter_numbers([1,2,3,4,5,6,7])
+
+	def test_for_the_function_functionality(makaveli) :
+		result = prove_of_concept_on_collection.filter_numbers([15,16,17,30,35,45,60,75,90,100,122,56])
+		makaveli.assertEqual(result, [15,30,45,60,75,90])
+
 	
+class TestForPalindromeFromWordsFunction(TestCase) :
+	def test_for_no_integers_in_the_function(makaveli) :
+		makaveli.assertRaises(ValueError, prove_of_concept_on_collection.is_palindrome, 7)
+
+	def test_for_Zero_input_in_the_function(makaveli) :
+		makaveli.assertRaises(ValueError, prove_of_concept_on_collection.is_palindrome, 0)
+
+	def test_for_negative_input_in_the_function(makaveli) :
+		makaveli.assertRaises(ValueError, prove_of_concept_on_collection.is_palindrome, -1)
+
+	def test_the_functon_functionality(makaveli) :
+		result = prove_of_concept_on_collection.is_palindrome("madam")
+		makaveli.assertNotEqual(result, "madam")
+
+
+class TestForPalindrome(TestCase) :
+	def test_that_the_function_only_accept_list(makaveli) :
+		makaveli.assertTrue(prove_of_concept_on_collection.check_for_palindrome (["words", "levels", "madam"]))
+
+	def test_for_no_integers_in_the_function(makaveli) :
+		makaveli.assertRaises(ValueError, prove_of_concept_on_collection.check_for_palindrome, ([2,3,4]))
+
+	def test_the_function_functionality(makaveli) :
+		result = prove_of_concept_on_collection.check_for_palindrome, (["words", "level", "madam"])
+		makaveli.assertNotEqual(result, ["level", "madam"])
+
+
+class TestFunctionThatConvertStringsToUpperCase(TestCase) :	
+	def test_that_the_function_only_accept_list(makaveli) :
+		makaveli.assertTrue(prove_of_concept_on_collection.convert_to_upper (["word", "class"]))
+
+	def test_that_the_function_accept_only_string(makaveli) :
+		makaveli.assertRaises(ValueError, prove_of_concept_on_collection.convert_to_upper, ([2,3,4,5,6]))
+
+	def test_the_function_functionality(makaveli) :
+		result = prove_of_concept_on_collection.convert_to_upper (["hello", "world"])
+		makaveli.assertEqual(result, ["HELLO", "WORLD"])
+
+
+class TestFunctionThatSquareAllNumberInARange(TestCase) :
+	def test_that_the_function_only_accept_list(makaveli) :
+		makaveli.assertTrue(  prove_of_concept_on_collection.square_number ([1,2,3,4,5,6,7,8,9,10]))
+
+	def test_that_the_function_dont_accept_anything_other_than_integers(makaveli) :
+		makaveli.assertRaises(ValueError, prove_of_concept_on_collection.square_number, (["welcome", 56.99]))
+
+	def test_the_function_functionality(makaveli) :
+		result = prove_of_concept_on_collection.square_number([1,2,3,4,5]) 
+		makaveli.assertEqual(result, [1,4,9,16,25])
+
+
+class TestFunctionThatCaplitalizedTheFirstLetterInAString(TestCase) :
+	def test_that_the_function_only_accept_list(makaveli) :
+		makaveli.assertTrue(prove_of_concept_on_collection.capitalize_first_letter (["Hello", "World", "Python"]))
+
+	def test_that_the_function_only_accept_strings(makaveli) :
+		makaveli.assertRaises(ValueError, prove_of_concept_on_collection.capitalize_first_letter, ([2,3,4,5,78.45,9.5]))
+
+	def test_the_function_functionality(makaveli) :
+		result = prove_of_concept_on_collection.capitalize_first_letter (["hello", "world"])
+		makaveli.assertEqual(result, ["Hello", "World"])
+		makaveli.assertNotEqual(result, ["hello", "world"])
+
+	
+class TestFunctionThatAdd10PercentTax(TestCase) :
+	def test_that_the_function_only_accept_list(makaveli) :
+		makaveli.assertTrue(prove_of_concept_on_collection.add_tax ([100,200,300]))
+
+	def test_that_the_function_only_accept_integers_and_float(makaveli) :
+		makaveli.assertRaises(ValueError, prove_of_concept_on_collection.add_tax, (["Hello", "Wolrd"]))
+
+	def test_the_function_functionality(makaveli) :
+		result = prove_of_concept_on_collection.add_tax ([100,200,300])
+		makaveli.assertNotEqual(result, [110.0, 220.0,330.0])
+
+
+class TestFunctionThatSumNumberInARangeWithReduceMethod(TestCase) :
+	def test_that_the_function_only_accept_list(makaveli) :
+		makaveli.assertTrue(prove_of_concept_on_collection.sum_all_numbers ([1,2,3,4,5,6,7,8,9,10]))
+
+	def test_that_the_function_only_accept_integers(makaveli) :
+		makaveli.assertRaises(ValueError, prove_of_concept_on_collection.sum_all_numbers, (["Hello", 34.9, 34.333, "world"]))
+
+	def test_the_function_functionality(makaveli) :
+		result = prove_of_concept_on_collection.sum_all_numbers ([1,2,3,4,5]) 
+		makaveli.assertEqual(result, 15)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
