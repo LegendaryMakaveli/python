da148deb2079324b447f59d21e21dd819c0d3f1e
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2025-09-20 16:06:27 +0100

    done

---
+++ Assignment/chapter_five/slicing.py
@@ -0,0 +1,15 @@
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+print("a)", alphabet[0:13])
+
+print("b)", alphabet[:13])
+
+print("c)", alphabet[13:26])
+
+print("d)", alphabet[13:])
+
+print("e)", alphabet[::2])
+
+print("f)", alphabet[::-1])
+
+print("g)", alphabet[::-1][::3])
