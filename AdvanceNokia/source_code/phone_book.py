81f9f4362ad4ef0d9d86529b9fe2d3e81d964e7b
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2025-09-27 13:23:47 +0100

    method to search for contact done

--- AdvanceNokia/source_code/phone_book.py
+++ AdvanceNokia/source_code/phone_book.py
@@ -4,3 +4,14 @@ class PhoneBook:
 
     def add_contact(self, name, contact):
         self.contact.append({"name": name, "contact": contact})
+
+    def search(self, name):
+        for log in self.contact:
+            if log["name"] == name:
+                return f"{log['name']}, {log['contact']}"
+        else :
+            raise NameError("Name not found")
+
+
+        
+
