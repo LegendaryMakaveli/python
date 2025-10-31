4c9b12af96d88fdf802bab8688e45ee99c97db22
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2025-10-31 07:09:39 +0100

    fuel with its method

--- multi_fuel_dispenser_system/source_code/fuel.py
+++ multi_fuel_dispenser_system/source_code/fuel.py
@@ -1,8 +1,8 @@
 class Fuel:
     def __init__(self, name, price_per_unit, available_quantity):
-        self._name = name
-        self._price_per_unit = price_per_unit
-        self._available_quantity = available_quantity
+        self.name = name
+        self.price_per_unit = price_per_unit
+        self.available_quantity = available_quantity
 
 
     @property
@@ -12,7 +12,7 @@ class Fuel:
     @name.setter
     def name(self, value):
         if not(isinstance(value, str)):
-            raise TypeError('The name must be a string')
+            raise TypeError("The name must be a string")
         self._name = value
 
     @property
@@ -22,7 +22,7 @@ class Fuel:
     @price_per_unit.setter
     def price_per_unit(self, value):
         if not isinstance(value, float):
-            raise TypeError('The price_per_unit must be a float')
+            raise TypeError("The price_per_unit must be a float")
         self._price_per_unit = value
 
     @property
@@ -32,12 +32,12 @@ class Fuel:
     @available_quantity.setter
     def available_quantity(self, value):
         if not isinstance(value, float):
-            raise ValueError('The available_quantity must be a float')
+            raise ValueError("The available_quantity must be a float")
         self._available_quantity = value
 
     def dispense(self, quantity):
         if not isinstance(quantity, float):
-            raise ValueError('The quantity must be a float')
+            raise ValueError("The quantity must be a float")
         if self.available_quantity > quantity:
             self.available_quantity -= quantity
             return True
@@ -46,18 +46,18 @@ class Fuel:
 
     def get_cost(self, quantity):
         if not isinstance(quantity, float):
-            raise ValueError('The quantity must be a float')
+            raise ValueError("The quantity must be a float")
             return self._price_per_unit * quantity
 
     def update_price(self, new_price):
         if not isinstance(new_price, int):
-            raise ValueError('The new price must be a int')
+            raise ValueError("The new price must be a int")
         if new_price > 10:
             self._price_per_unit = new_price
 
     def restock(self, amount):
         if not isinstance(amount, float):
-            raise ValueError('The amount must be a float')
+            raise ValueError("The amount must be a float")
         if amount >= 100:
             self._available_quantity += amount
 
